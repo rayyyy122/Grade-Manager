946793898ccc47b6e36a14064b1b579dd21fad05
ray <tdr55260@gmail.com>
2022-03-23 15:45:34 -0700

    project phase 3 version 2.3.1: continue to add tests to satisfy coverage

--- src/main/ui/ManageGradePanel.java
+++ src/main/ui/ManageGradePanel.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 public class ManageGradePanel extends JPanel implements ActionListener {
     protected JButton add;
     protected JButton delete;
-    protected JButton change = new JButton("change a grade");
     protected JButton button = new JButton("back");
     public static final JFrame frame = new JFrame("manage my grade");
 
@@ -18,10 +17,6 @@ public class ManageGradePanel extends JPanel implements ActionListener {
         add.setHorizontalTextPosition(AbstractButton.CENTER);
         add.addActionListener(this);
         add(add);
-        change.setVerticalTextPosition(AbstractButton.CENTER);
-        change.setHorizontalTextPosition(AbstractButton.CENTER);
-        change.addActionListener(this);
-        add(change);
         delete = new JButton("delete a course");
         delete.setVerticalTextPosition(AbstractButton.CENTER);
         delete.setHorizontalTextPosition(AbstractButton.CENTER);
